141ac178bfa834d411eb56e505539459f4face65
yisen <354510585@qq.com>
2019-08-10 11:36:02 +0800

    修改MiaoshaUser

--- miaosha/src/main/java/com/yisen/miaosha/domain/MiaoshaUser.java
+++ miaosha/src/main/java/com/yisen/miaosha/domain/MiaoshaUser.java
@@ -3,17 +3,6 @@ package com.yisen.miaosha.domain;
 import java.util.Date;
 
 public class MiaoshaUser {
-/*	CREATE TABLE `miaosha_user` (
-			`id` bigint(20) NOT NULL COMMENT '用户id,手机号码',
-			`nickname` varchar(255) NOT NULL COMMENT '昵称',
-			`password` varchar(32) DEFAULT NULL COMMENT '密码，两次md5',
-			`salt` varchar(10) DEFAULT NULL COMMENT '随机值',
-			`head` varchar(128) DEFAULT NULL COMMENT '头像，云存储的id',
-			`register_date` datetime DEFAULT NULL COMMENT '注册时间',
-			`last_login_date` datetime DEFAULT NULL COMMENT '上次登录时间',
-			`login_count` int(11) DEFAULT NULL COMMENT '登录次数',
-	PRIMARY KEY (`id`)
-	) ENGINE=InnoDB DEFAULT CHARSET=utf8;*/
 
 	private Long id;         //用户id,手机号码
 	private String nickname; //昵称
